1a3e06f2fb434a3ef4717a44aad0e76753081dee
Michael Ring <mail@michael-ring.org>
2025-07-17 13:24:22 +0200

    Added script to monitor and restart Allsky Camera

--- Client/allskyMonitor.py
+++ Client/allskyMonitor.py
@@ -19,9 +19,9 @@ lastdate=datetime.fromisoformat(lastdatestr)
 currentdate=datetime.now()
 if currentdate > lastdate+timedelta(minutes=3):
   logger.info('Last log entry is older than 3 minutes, restarting allsky')
-  logger.info('contents[-3]')
-  logger.info('contents[-2]')
-  logger.info('contents[-1]')
+  logger.info(contents[-3])
+  logger.info(contents[-2])
+  logger.info(contents[-1])
   subprocess.run('/usr/bin/sudo /usr/bin/systemctl restart allsky'.split(' '))
 
 
