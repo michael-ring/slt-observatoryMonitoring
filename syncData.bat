2864e909e3042a7d5405a6408e8cb0f7ae0473dd
Michael Ring <mail@michael-ring.org>
2026-03-27 19:06:40 -0300

    added new scripts for multi scope cases

--- syncData.bat
+++ syncData.bat
@@ -3,4 +3,4 @@ call "%HOMEDRIVE%%HOMEPATH%\devel\pyBlink\createCache.bat
 cd "%HOMEDRIVE%%HOMEPATH%\devel\slt-observatoryMonitoring\"
 call  "%HOMEDRIVE%%HOMEPATH%\devel\slt-observatoryMonitoring\venv\Scripts\activate.bat"
 set PYTHONPATH=%PYTHONPATH%;"%HOMEDRIVE%%HOMEPATH%\devel\slt-observatoryMonitoring\"
-python.exe  "%HOMEDRIVE%%HOMEPATH%\devel\slt-observatoryMonitoring\Client\telescopeDataSync.py"
+python.exe  "%HOMEDRIVE%%HOMEPATH%\devel\slt-observatoryMonitoring\Client\telescopeMultiDataSync.py"
